ea5625df288cded0f01cee904753cfe07d9b87ce
pritomnyc <mahim.pritom@gmail.com>
2021-07-21 22:47:13 -0400

    sliders

---
+++ frontend/src/components/SliderData.js
@@ -0,0 +1,10 @@
+export const SliderData = [
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0188/0122/t/10/assets/carousel-item-3.jpg?v=13472218463533484032",
+  },
+  {
+    image:
+      "https://cdn.shopify.com/s/files/1/0188/0122/t/10/assets/carousel-item-1.jpg?v=16780820781792735853",
+  },
+];
