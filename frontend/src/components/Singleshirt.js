8e5c0ac8f98db76b7ce1d7dc942df56425fb5a9c
pritomnyc <mahim.pritom@gmail.com>
2021-07-22 16:45:52 -0400

    single changes

--- frontend/src/components/Singleshirt.js
+++ frontend/src/components/Singleshirt.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import actions from "../api";
+import { Link } from "react-router-dom";
 
 function Singleshirt(props) {
   const [singleShirt, setSingleShirt] = useState();
@@ -12,11 +13,49 @@ function Singleshirt(props) {
   }, []);
 
   return (
-    <div className="singlePage">
-      <div>
-        <img src={singleShirt?.imageUrl[0]} alt="single shirt pic" />
-        Hello
-        {/* {singleShirt.club} */}
+    <div className="singleShirtContainer">
+      <div className="singleShirtFlex">
+        <img
+          style={{ width: `100%` }}
+          src={singleShirt?.imageUrl[0]}
+          alt="single-shirt-pic"
+        />
+      </div>
+      <div className="singleDescription">
+        <ul>
+          <li>
+            Club: <span style={{ color: `maroon` }}>{singleShirt?.club}</span>
+          </li>
+          <li>
+            Year: <span style={{ color: `maroon` }}>{singleShirt?.year}</span>
+          </li>
+          <li>
+            Player:{" "}
+            <span style={{ color: `maroon` }}>{singleShirt?.player}</span>
+          </li>
+          <li>
+            League:{" "}
+            <span style={{ color: `maroon` }}>{singleShirt?.league}</span>
+          </li>
+          <li>
+            Size: <span style={{ color: `maroon` }}>{singleShirt?.size}</span>
+          </li>
+          <li>
+            Condition:{" "}
+            <span style={{ color: `maroon` }}>{singleShirt?.condition}</span>
+          </li>
+          <li>
+            Price: $<span style={{ color: `red` }}>{singleShirt?.price}</span>
+          </li>
+          <li>
+            Notes: <span style={{ color: `maroon` }}>{singleShirt?.notes}</span>
+          </li>
+          <li>
+            <Link to="/Mycart">
+              <button className="mylist-popbutton">➕ Add to Cart</button>
+            </Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
